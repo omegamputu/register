fed7f9ed87b96950df355f06d14f8d90d9dd49b9
Omega Mputu <kutekemeni93@gmail.com>
2020-11-16 06:56:24 -0800

    La page de reunitialisation crée

---
+++ forget.php
@@ -0,0 +1,18 @@
+<?php include 'inc/header.php'; ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-5">
+			<h1>Mot de passe oublié</h1>
+			<form action="" method="post">
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" id="email" name="email" class="form-control">
+				</div>
+				<button type="submit" class="btn btn-primary">Envoyer</button>
+			</form>
+		</div>
+	</div>
+</div>
+
+<?php include 'inc/footer.php'; ?>
